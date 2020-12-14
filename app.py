a79250cd23e3b84e32f6230e92aec9f92383dac4
joshuasuzuki <joshuakai.suzuki@gmail.com>
2020-12-14 01:04:16 -0500

    create hand model

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, Response,jsonify
 from database.db import initialize_db
-from database.models import Session
+from database.models import Session,Hand
 
 app = Flask(__name__)
 
@@ -20,16 +20,19 @@ test_session = {
 def hello():
     return "HelloGoodbye"
 
+# All Sessions
 @app.route('/Sessions')
 def get_sessions():
     all_sessions = Session.objects().to_json()
     return Response(all_sessions,mimetype="application/jason",status=200)
 
+# Get Session by id
 @app.route('/Sessions/<id>')
 def get_session_by_id(id):
     session = Session.objects.get(id=id).to_json()
     return Response(session,mimetype="application/json",status=200)
 
+# Add new Session
 @app.route('/Sessions',methods=['POST'])
 def add_session():
     body = request.get_json()
@@ -38,15 +41,25 @@ def add_session():
     return {'id':str(id)}, 200
     return jsonify(body)
 
+# Update Session
 @app.route('/Sessions/<id>',methods=['PUT'])
 def update_session(id):
     body = request.get_json()
     Session.objects.get(id=id).update(**body)
     return 'Session '+id+' updated',200
 
+# Delete Session
 @app.route('/Sessions/<id>',methods=['DELETE'])
 def delete_session(id):
     Session.objects.get(id=id).delete()
     return 'Session deleted ',200
 
+# Get all Hands by Session id
+@app.route('/Session/<id>/Hands')
+def get_hands_by_session(id):
+    hands = Hand.objects().get(session_id=id).to_json()
+    return Response(hands,mimetype="application/jason",status=200)
+
+@app.route('/Session/<id>/Hands',methods=['POST'])
+def new_hand(id):
 app.run()
--- database/models.py
+++ database/models.py
@@ -1,8 +1,17 @@
 from .db import db
 import datetime
 
+class Hand(db.Document):
+    session_id = db.StringField(required=True)
+    hole_cards = db.ListField(db.StringField(),required=True)
+    community_cards = db.ListField(db.StringField())
+    action = db.ListField(db.StringField())
+    starting_stack = db.IntField(default=0)
+    ending_stack = db.IntField(default=0)
+
 class Session(db.Document):
     location = db.StringField(required=True)
     date = db.DateTimeField(default=datetime.datetime.now())
     start_money = db.IntField(default=0)
     end_money = db.IntField(default=0)
+    hands = EmbeddedDocumentListField('Hand')
